f4f3edc74c8241eaa8d9cb8ce93477f64a5910ba
byplayer <byplayer100@gmail.com>
2011-01-07 14:38:46 +0900

    load plugin config

--- init.sh
+++ init.sh
@@ -2,3 +2,9 @@ export ZSH_EXT_BASE=$HOME/.zsh.d
 
 # TIP: Add files you don't want in git to .gitignore
 for config_file ($ZSH_EXT_BASE/lib/*.zsh) source $config_file
+
+# load plugins
+for plugin ($ZSH_EXT_BASE/plugins/*) {
+  plugin_name=`basename $plugin`
+  source $ZSH_EXT_BASE/plugins/$plugin_name/$plugin_name.plugin.zsh
+}
