9408aa394f4b435017afb4cc08b3710c3268928c
byplayer <byplayer100@gmail.com>
2011-01-07 15:07:13 +0900

    move update compinit from gem plugin to init.sh

--- init.sh
+++ init.sh
@@ -8,3 +8,7 @@ for plugin ($ZSH_EXT_BASE/plugins/*) {
   plugin_name=`basename $plugin`
   source $ZSH_EXT_BASE/plugins/$plugin_name/$plugin_name.plugin.zsh
 }
+
+autoload -U compinit
+compinit -i
+
--- plugins/gem/gem.plugin.zsh
+++ plugins/gem/gem.plugin.zsh
@@ -1,4 +1,1 @@
-# add brew completion function to path
 fpath=($ZSH_EXT_BASE/plugins/gem $fpath)
-autoload -U compinit
-compinit -i
