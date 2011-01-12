234afaf54359837d4c47d26e61ab1771baee9cb0
byplayer <byplayer100@gmail.com>
2011-01-12 11:42:15 +0900

    change alias ructags to rbctags

--- lib/ruby.zsh
+++ lib/ruby.zsh
@@ -4,7 +4,7 @@ RUBY_HOME=/usr/local/ruby
 export PATH=${RUBY_HOME}/bin:$PATH
 export MANPATH=${RUBY_HOME}/share/man:`manpath -q`
 
-alias ructags="ctags --regex-ruby='/^[ \t]*([A-Z_][A-Z0-9_]*)[ \t]*=/\1/C,constant/'"
+alias rbctags="ctags --regex-ruby='/^[ \t]*([A-Z_][A-Z0-9_]*)[ \t]*=/\1/C,constant/'"
 
 # rails env
 alias r="rails"
