d0b4eaf27c232658501929787983e73337782e6f
byplayer <byplayer100@gmail.com>
2011-01-12 11:38:59 +0900

    add ctags option for ruby constant .

--- lib/ruby.zsh
+++ lib/ruby.zsh
@@ -4,5 +4,7 @@ RUBY_HOME=/usr/local/ruby
 export PATH=${RUBY_HOME}/bin:$PATH
 export MANPATH=${RUBY_HOME}/share/man:`manpath -q`
 
+alias ructags="ctags --regex-ruby='/^[ \t]*([A-Z_][A-Z0-9_]*)[ \t]*=/\1/C,constant/'"
+
 # rails env
 alias r="rails"
