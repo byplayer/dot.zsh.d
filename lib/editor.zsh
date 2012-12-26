b557b927fb8c42ea10a56dfcf2b3fc7e97fd4816
byplayer <byplayer100@gmail.com>
2012-12-26 11:25:27 +0900

    update editor configuration

--- lib/editor.zsh
+++ lib/editor.zsh
@@ -1,12 +1,16 @@
 # emacs config
 # alias emacs='XMODIERS="@im=none" LC_CTYPE="ja_JP.utf8" emacs 2>/dev/null'
 alias emacs='XMODIERS="@im=none" emacs 2>/dev/null'
-EDITOR=/usr/bin/emacsclient
-export EDITOR
-
-export ALTERNATE_EDITOR="emacs"
 
 alias ec="emacsclient"
 alias ecn='emacsclient -n'
 
 export PATH=$PATH:/usr/local/SublimeText
+alias sub=sublime_text
+
+EDITOR=/usr/bin/emacsclient
+export EDITOR
+export ALTERNATE_EDITOR="emacs"
+
+# EDITOR='sublime_text --wait'
+# export EDITOR
