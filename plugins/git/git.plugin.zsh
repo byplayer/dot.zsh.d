e526dd4ffcf8d657842531d7b8b6a5c9a4ffcf53
byplayer <byplayer100@gmail.com>
2013-03-21 11:31:57 +0900

    update git completion

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -3,5 +3,4 @@ export PATH=/usr/local/git/bin:~/.git-extensions/bin:$PATH
 export MANPATH=/usr/local/git/share/man:`manpath -q`
 alias g="git"
 
-fpath=($ZSH_EXT_BASE/plugins/git $fpath)
-
+source ${ZSH_EXT_BASE}/plugins/git/git-completion.bash
