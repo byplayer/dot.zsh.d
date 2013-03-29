3bbd0eadfc2af749a2b14d7782de88833b2e66cd
byplayer <byplayer100@gmail.com>
2013-03-29 09:26:17 +0900

    Revert "update git completion"
    This reverts commit e526dd4ffcf8d657842531d7b8b6a5c9a4ffcf53.

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -3,4 +3,5 @@ export PATH=/usr/local/git/bin:~/.git-extensions/bin:$PATH
 export MANPATH=/usr/local/git/share/man:`manpath -q`
 alias g="git"
 
-source ${ZSH_EXT_BASE}/plugins/git/git-completion.bash
+fpath=($ZSH_EXT_BASE/plugins/git $fpath)
+
