30801dc37e146b53b8b999e4377cb9a99a65ee3b
byplayer <byplayer100@gmail.com>
2013-06-26 17:25:20 +0900

    use cdr

---
+++ lib/cdr.zsh
@@ -0,0 +1,5 @@
+autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+add-zsh-hook chpwd chpwd_recent_dirs
+zstyle ':chpwd:*' recent-dirs-max 5000
+zstyle ':chpwd:*' recent-dirs-default yes
+zstyle ':completion:*' recent-dirs-insert both
