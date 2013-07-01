f7427bf2242625af83b762d27a196a742f016c41
byplayer <byplayer100@gmail.com>
2013-07-01 15:27:13 +0900

    change zaw history binding

--- lib/zaw.zsh
+++ lib/zaw.zsh
@@ -1,7 +1,7 @@
 bindkey '^gz' zaw
 bindkey '^gb' zaw-git-recent-all-branches
 bindkey '^gs' zaw-git-status
-bindkey '^gh' zaw-history
+bindkey '^r' zaw-history
 bindkey '^gc' zaw-cdr
 
 export ZAW_EDITOR='emacsclient -n'
