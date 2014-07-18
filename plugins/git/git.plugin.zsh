1bff7704ae513a23462c592f91d0ab52c9a82645
byplayer <byplayer100@gmail.com>
2014-07-18 16:58:09 +0800

    change git dir

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -1,6 +1,6 @@
 # git env
-export PATH=/usr/local/git/bin:~/.git-extensions/bin:$PATH
-export MANPATH=/usr/local/git/share/man:`manpath -q`
+export PATH=/opt/git/bin:~/.git-extensions/bin:$PATH
+export MANPATH=/opt/git/share/man:`manpath -q`
 alias g="git"
 
 fpath=($ZSH_EXT_BASE/plugins/git $fpath)
