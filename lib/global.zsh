28d126d049ecb388ca41ae92c2cb6fafff18bbb1
byplayer <byplayer100@gmail.com>
2014-07-24 15:43:46 +0800

    change global path

--- lib/global.zsh
+++ lib/global.zsh
@@ -1,2 +1,2 @@
-export PATH=/usr/local/global/bin:$PATH
-export MANPATH=/usr/local/global/share/man:`manpath -q`
+export PATH=/opt/global/bin:$PATH
+export MANPATH=/opt/global/share/man:`manpath -q`
