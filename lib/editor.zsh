be8bb7663721d341f97152e793c78eda530d302d
byplayer <byplayer100@gmail.com>
2014-07-29 12:37:16 +0800

    fix typo

--- lib/editor.zsh
+++ lib/editor.zsh
@@ -1,6 +1,6 @@
 # emacs config
 # alias emacs='XMODIERS="@im=none" LC_CTYPE="ja_JP.utf8" emacs 2>/dev/null'
-alias emacs='XMODIERS="@im=none" emacs 2>/dev/null'
+alias emacs='XMODFIERS="@im=none" emacs 2>/dev/null'
 
 alias ec="emacsclient"
 alias ecn='emacsclient -n'
