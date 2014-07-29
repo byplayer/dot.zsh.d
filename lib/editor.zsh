d60c482c210d56c8a36eab57f2fe6797c149cf37
byplayer <byplayer100@gmail.com>
2014-07-29 10:06:30 +0800

    remove sublime text configuration

--- lib/editor.zsh
+++ lib/editor.zsh
@@ -5,12 +5,6 @@ alias emacs='XMODIERS="@im=none" emacs 2>/dev/null'
 alias ec="emacsclient"
 alias ecn='emacsclient -n'
 
-export PATH=$PATH:/usr/local/SublimeText
-alias sub=sublime_text
-
 EDITOR=/usr/bin/emacsclient
 export EDITOR
 export ALTERNATE_EDITOR="emacs"
-
-# EDITOR='sublime_text --wait'
-# export EDITOR
