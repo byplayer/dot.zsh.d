d31bdae8823326e621d7d0bb0e01689fb8b5cc55
Yukio Goto <yukio.goto@rakuten.com>
2015-09-01 11:47:54 +0800

    cleanup java env

--- lib/gvm.zsh
+++
@@ -1,5 +0,0 @@
-#THIS MUST BE AT THE END OF THE FILE FOR GVM TO WORK!!!
-[[ -s "$HOME/.gvm/bin/gvm-init.sh" ]] && source "$HOME/.gvm/bin/gvm-init.sh"
-
-# fix problem for zaw-src-cdr
-unsetopt sh_word_split
--- lib/java.zsh
+++ lib/java.zsh
@@ -1,3 +1,8 @@
 if [ -f $HOME/.jdk_default.sh ]; then
   source $HOME/.jdk_default.sh
 fi
+
+[[ -s "$HOME/.gvm/bin/gvm-init.sh" ]] && source "$HOME/.gvm/bin/gvm-init.sh"
+
+# fix problem for zaw-src-cdr
+unsetopt sh_word_split
