33d2e234b56a7dd52e67976d03b4cf92c3cd6566
Yukio Goto <yukio.goto@rakuten.com>
2016-04-11 11:39:24 +0800

    use rbenv

--- lib/ruby.zsh
+++ lib/ruby.zsh
@@ -1,13 +1,11 @@
-# ruby environment
-RUBY_HOME=/usr/local/ruby
+#rbenv
+export RBENV_ROOT=/opt/rbenv
+export PATH=${RBENV_ROOT}/bin:$PATH
+eval "$(rbenv init -)"
 
-export PATH=${RUBY_HOME}/bin:$PATH
-export MANPATH=${RUBY_HOME}/share/man:`manpath -q`
-
-alias rbctags="ctags --regex-ruby='/^[ \t]*([A-Z_][A-Z0-9_]*)[ \t]*=/\1/C,constant/'"
-
-# rails env
+# ruby aliases
 alias r="rails"
 alias be="bundle exec"
 alias berails="bundle exec rails"
 alias berake="bundle exec rake"
+
