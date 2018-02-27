9f7ec9c0ae8d786551738ecb8d266e2ed17f3a4b
byplayer <byplayer100@gmail.com>
2018-02-27 19:00:15 +0800

    update docker alias

--- plugins/docker/docker.plugin.zsh
+++ plugins/docker/docker.plugin.zsh
@@ -1,10 +1,10 @@
 # docker
 alias d="docker"
 
-# drma() { docker rm $(docker ps -a -q); }
-alias dpa="docker ps -a"
-alias dps="docker ps"
-alias di="docker images"
+alias dcl="docker container ls"
+alias dcla="docker container ls -a"
+alias dil="docker image ls"
+alias dip="docker image prune"
 alias dc="docker-compose"
 
 dalias() { alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/['|\']//g" | sort;}
