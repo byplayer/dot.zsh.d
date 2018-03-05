05379d1afdf11f9ed28693425098c5ab5193fa6d
byplayer <byplayer100@gmail.com>
2018-03-05 16:26:47 +0800

    move docker sub command alias from shell to salias

--- plugins/docker/docker.plugin.zsh
+++ plugins/docker/docker.plugin.zsh
@@ -1,10 +1,4 @@
 # docker
 alias d="docker"
 
-alias dcl="docker container ls"
-alias dcla="docker container ls -a"
-alias dil="docker image ls"
-alias dip="docker image prune"
-alias dc="docker-compose"
-
 dalias() { alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/['|\']//g" | sort;}
