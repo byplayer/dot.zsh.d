1a7d9b286e0890abed7454e84c1e14568cbf11c8
byplayer <byplayer100@gmail.com>
2018-03-06 09:39:25 +0800

    add additional aliases

--- plugins/docker/docker.plugin.zsh
+++ plugins/docker/docker.plugin.zsh
@@ -1,4 +1,43 @@
 # docker
 alias d="docker"
 
+docker() {
+  case $1 in
+    i.l)
+      shift
+      command docker image ls "$@"
+      ;;
+    i.p)
+      shift
+      command docker image prune "$@"
+      ;;
+    i.r)
+      shift
+      command docker image rm "$@"
+      ;;
+    i)
+      shift
+      command docker image "$@"
+      ;;
+    c.l)
+      shift
+      command docker container ls "$@"
+      ;;
+    c.p)
+      shift
+      command docker container prune "$@"
+      ;;
+    c.r)
+      shift
+      command docker container rm "$@"
+      ;;
+    c)
+      shift
+      command docker container "$@"
+      ;;
+    *)
+      command docker "$@";;
+  esac
+}
+
 dalias() { alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/['|\']//g" | sort;}
