3cfe05e091d6260fa2bff560d475f2b99f5fb567
byplayer <byplayer100@gmail.com>
2019-04-30 12:54:56 +0800

    update git completion

--- plugins/git/git.plugin.zsh
+++
@@ -1,7 +0,0 @@
-# git env
-export PATH=/opt/git/bin:/opt/git/contrib/diff-highlight:~/.git-extensions/bin:$PATH
-export MANPATH=/opt/git/share/man:`manpath -q`
-alias g="git"
-
-fpath=($ZSH_EXT_BASE/plugins/git $fpath)
-
