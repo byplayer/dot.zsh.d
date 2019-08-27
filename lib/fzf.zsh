d3831b3ec7350acff1def23457c263bf56865c67
byplayer <byplayer100@gmail.com>
2019-08-27 09:09:41 +0800

    :+1: use fzf for cdr
    Use fzf cdr because fzf incremental search is better than zaw.

--- lib/fzf.zsh
+++ lib/fzf.zsh
@@ -1,2 +1,15 @@
 export PATH=/opt/fzf/bin:$PATH
 export MANPATH=/opt/fzf/man:`manpath -q`
+
+export FZF_DEFAULT_OPTS='--color=fg+:11 --height 70% --reverse --select-1 --exit-0 --multi'
+
+function fzf-cdr () {
+  local selected_dir=$(cdr -l | awk '{ print $2 }' | fzf --query "$LBUFFER")
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
+  fi
+  zle clear-screen
+}
+zle -N fzf-cdr
+bindkey "^gc" fzf-cdr
--- lib/zaw.zsh
+++ lib/zaw.zsh
@@ -2,7 +2,6 @@ bindkey '^gz' zaw
 bindkey '^gb' zaw-git-recent-all-branches
 bindkey '^gs' zaw-git-status
 bindkey '^r' zaw-history
-bindkey '^gc' zaw-cdr
 
 export ZAW_EDITOR='emacsclient -n'
 
