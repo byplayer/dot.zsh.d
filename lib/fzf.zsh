490e8363ac8055036200c12d97ba259e52cb809f
Yukio Goto <yukio.goto@rakuten.com>
2019-08-27 10:00:49 +0800

    :+1: Change fzf color scheme
    To change mood

--- lib/fzf.zsh
+++ lib/fzf.zsh
@@ -1,7 +1,10 @@
 export PATH=/opt/fzf/bin:$PATH
 export MANPATH=/opt/fzf/man:`manpath -q`
 
-export FZF_DEFAULT_OPTS='--color=fg+:11 --height 70% --reverse --select-1 --exit-0 --multi'
+export FZF_DEFAULT_OPTS='--color=dark
+--color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,hl+:#d858fe
+--color=info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:150,header:#61afef
+--reverse --select-1 --exit-0 --multi'
 
 function fzf-cdr () {
   local selected_dir=$(cdr -l | awk '{ print $2 }' | fzf --query "$LBUFFER")
