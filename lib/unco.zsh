2aadfe2589e486b48bd4481d341fd6c47b1150d9
byplayer <byplayer100@gmail.com>
2019-09-25 09:35:49 +0800

    :sparkles: Add accept-line hook to use unco record for git and rm
    If I use alias git="unco record -- git"
    I cannot use zsh completion.
    So I add hook function.

--- lib/unco.zsh
+++ lib/unco.zsh
@@ -1,6 +1,23 @@
 export PATH=/opt/unco/bin:$PATH
 export MANPATH=/opt/unco/share/man:`manpath -q`
 
-alias g="unco record -- git"
-alias git="unco record -- git"
-alias rm="unco record -- rm"
+# add hook to use unco record --
+# If I use alias git="unco record -- git"
+# I cannot use zsh completion.
+# so I add unco hook
+UNCO_HOOK_CMDS=(git rm)
+unco-hook-accept-line () {
+  if [[ "$BUFFER" == "g "* ]]; then
+    BUFFER="unco record -- git ${BUFFER##g }"
+  else
+    for c in $UNCO_HOOK_CMDS; do
+      if [[ "$BUFFER" == "$c "* ]]; then
+        BUFFER="unco record -- ${BUFFER}"
+      fi
+    done
+  fi
+
+  zle .accept-line
+}
+
+zle -N accept-line unco-hook-accept-line
