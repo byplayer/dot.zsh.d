e6fa04d5260bb934c552e4b504ec0d29bf700402
byplayer <byplayer100@gmail.com>
2020-03-06 02:09:41 +0900

    :+1: Unused unco because some completion error

--- plugins/unco/unco.plugin.zsh
+++ plugins/unco/unco.plugin.zsh
@@ -6,19 +6,19 @@ fpath=($ZSH_EXT_BASE/plugins/unco $fpath)
 # If I use alias git="unco record -- git"
 # I cannot use zsh completion.
 # so I add unco hook
-UNCO_HOOK_CMDS=(git rm)
-unco-hook-accept-line () {
-  if [[ "$BUFFER" == "g "* ]]; then
-    BUFFER="unco record -- git ${BUFFER##g }"
-  else
-    for c in $UNCO_HOOK_CMDS; do
-      if [[ "$BUFFER" == "$c "* ]]; then
-        BUFFER="unco record -- ${BUFFER}"
-      fi
-    done
-  fi
+# UNCO_HOOK_CMDS=(git rm)
+# unco-hook-accept-line () {
+#   if [[ "$BUFFER" == "g "* ]]; then
+#     BUFFER="unco record -- git ${BUFFER##g }"
+#   else
+#     for c in $UNCO_HOOK_CMDS; do
+#       if [[ "$BUFFER" == "$c "* ]]; then
+#         BUFFER="unco record -- ${BUFFER}"
+#       fi
+#     done
+#   fi
 
-  zle .accept-line
-}
+#   zle .accept-line
+# }
 
-zle -N accept-line unco-hook-accept-line
+# zle -N accept-line unco-hook-accept-line
