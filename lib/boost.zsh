0670654729f14a472304d44f5a4dd09511e83524
byplayer <byplayer100@gmail.com>
2020-09-05 01:41:41 +0900

    :+1: commented out boost configuration
    I don't use boost now.

--- lib/boost.zsh
+++ lib/boost.zsh
@@ -1,5 +1,5 @@
 # Environment configuration for boost library
 
-export CPLUS_INCLUDE_PATH=/opt/boost/include:$CPLUS_INCLUDE_PATH
-export LD_LIBRARY_PATH=/opt/boost/lib:${LD_LIBRARY_PATH}
-export LIBRARY_PATH=/opt/boost/lib:${LIBRARY_PATH}
+# export CPLUS_INCLUDE_PATH=/opt/boost/include:$CPLUS_INCLUDE_PATH
+# export LD_LIBRARY_PATH=/opt/boost/lib:${LD_LIBRARY_PATH}
+# export LIBRARY_PATH=/opt/boost/lib:${LIBRARY_PATH}
