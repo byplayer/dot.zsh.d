8e9ce2b71a9748efda7225ace24773edb4c1727c
byplayer <byplayer100@gmail.com>
2021-02-12 18:16:57 +0900

    :+1: Add diff-highlight path on macos

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -3,4 +3,10 @@ export PATH=/opt/git/bin:/opt/git/contrib/diff-highlight:~/.git-extensions/bin:$
 export MANPATH=/opt/git/share/man:`manpath -q`
 alias g="git"
 
+case ${OSTYPE} in
+  darwin*)
+    export PATH=/usr/local/opt/git/share/git-core/contrib/diff-highlight/:$PATH
+    ;;
+esac
+
 fpath=($ZSH_EXT_BASE/plugins/git $fpath)
