4b02eee0a2e55d6de359c2ed6f37f6704c91336b
byplayer <byplayer100@gmail.com>
2021-06-15 15:16:22 +0900

    👍 support new pyenv configuration
    pyenv configuration is changed so I support it.

--- lib/python.zsh
+++ lib/python.zsh
@@ -1,6 +1,8 @@
 export PATH=$HOME/.python_tool/bin:$PATH
 if [ -d $HOME/.pyenv ]; then
+  export PYENV_ROOT="$HOME/.pyenv"
   export PATH=$HOME/.pyenv/bin:$PATH
 fi
-eval "$(pyenv init -)"
+
+eval "$(pyenv init --path)"
 eval "$(pyenv virtualenv-init -)"
