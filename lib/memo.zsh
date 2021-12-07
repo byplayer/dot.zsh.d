f5f66f0216c231fbedeaac8944d33517c9a060f4
byplayer <byplayer100@gmail.com>
2021-12-07 10:42:51 +0900

    👍 add parameter check for make_memo
    Add usage and parameter check for make_memo function.

--- lib/memo.zsh
+++ lib/memo.zsh
@@ -3,11 +3,16 @@ export MEMO_EDITOR=code
 
 function make_memo () {
     MEMO_BASE_NAME=$1
-
+    
+    if [ -z "$MEMO_BASE_NAME" ] ; then
+        echo "usage: make_memo memo_title"
+        return 1
+    fi
+    
     if [ ! -d ${MEMO_BASE_DIR}/docs/`date +%Y` ] ; then
         mkdir ${MEMO_BASE_DIR}/docs/`date +%Y`
     fi
-
+    
     MEMO_PATH=${MEMO_BASE_DIR}/docs/`date +%Y`/`date +%Y%m%d`_${MEMO_BASE_NAME}.md
     echo "create and open ${MEMO_PATH}"
     ${MEMO_EDITOR} $MEMO_PATH
