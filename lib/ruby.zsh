026586b14811ddcc4abc76e0640d27d8302ed273
byplayer <byplayer100@gmail.com>
2021-12-08 13:12:43 +0900

    👍 switch ruby_tool path from user home to /opt
    Change ruby_tool path from user home to /opt.

--- lib/ruby.zsh
+++ lib/ruby.zsh
@@ -1,15 +1,14 @@
 #rbenv
 if [ -d /opt/rbenv ]; then
-  export RBENV_ROOT=/opt/rbenv
-  export PATH=${RBENV_ROOT}/bin:$PATH
+    export RBENV_ROOT=/opt/rbenv
+    export PATH=${RBENV_ROOT}/bin:$PATH
 fi
 eval "$(rbenv init -)"
 
-export PATH=$HOME/.ruby_tool/bin:$PATH
+export PATH=/opt/ruby_tool/bin:$PATH
 
 # ruby aliases
 alias r="rails"
 alias be="bundle exec"
 alias berails="bundle exec rails"
 alias berake="bundle exec rake"
-
