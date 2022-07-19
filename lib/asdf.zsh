c85870356c3f3258ec38508ab5ff07c48832b475
byplayer <byplayer100@gmail.com>
2022-07-19 13:01:58 +0900

    👍 Add JAVA_HOME configuration for asdf java plugin
    I add to set JAVA_HOME using asdf java plugin.

--- lib/asdf.zsh
+++ lib/asdf.zsh
@@ -1,5 +1,9 @@
 type brew > /dev/null
 if [ $? -eq 0 ]; then
-    source $(brew --prefix asdf)/libexec/asdf.sh
-    fpath=(/opt/homebrew/opt/asdf/share/zsh/site-functions $fpath)
+  source $(brew --prefix asdf)/libexec/asdf.sh
+  fpath=(/opt/homebrew/opt/asdf/share/zsh/site-functions $fpath)
+fi
+
+if [ -f  ~/.asdf/plugins/java/set-java-home.zsh ]; then
+  source ~/.asdf/plugins/java/set-java-home.zsh
 fi
