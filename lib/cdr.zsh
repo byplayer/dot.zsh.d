42b8e02e5aed94598ecb6910c01a8493393af911
byplayer <byplayer100@gmail.com>
2023-03-20 14:28:41 +0900

    👍 add clean up not exist dir from cdr
    I add cleaning up hook and function when cd.
    I use fzf + cdr to switch dir but there are a lot of removed
    dir in list. So I add hook to clean up not exist dir automatically
    from cdr list.
    
    https://blog.n-z.jp/blog/2014-07-25-compact-chpwd-recent-dirs.html

--- lib/cdr.zsh
+++ lib/cdr.zsh
@@ -3,3 +3,19 @@ zstyle ':completion:*' recent-dirs-insert both
 zstyle ':chpwd:*' recent-dirs-max 5000
 zstyle ':chpwd:*' recent-dirs-default true
 add-zsh-hook chpwd chpwd_recent_dirs
+
+# clean up not exist dir from cdr
+my-compact-chpwd-recent-dirs () {
+    emulate -L zsh
+    setopt extendedglob
+    local -aU reply
+    integer history_size
+    autoload -Uz chpwd_recent_filehandler
+    chpwd_recent_filehandler
+    history_size=$#reply
+    reply=(${^reply}(N))
+    (( $history_size == $#reply )) || chpwd_recent_filehandler $reply
+}
+
+# call clean up cdr when cd
+add-zsh-hook chpwd my-compact-chpwd-recent-dirs
