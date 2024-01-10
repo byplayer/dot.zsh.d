02eac943351f12b1d09a6ed45b28294c219a54d6
byplayer <byplayer100@gmail.com>
2024-01-10 15:27:25 +0900

    👍 add '-F' option for eza(ls)

--- lib/misc.zsh
+++ lib/misc.zsh
@@ -57,7 +57,7 @@ EZA_COLORS+=":*.rb=38;5;124:*.md=38;5;52"
 EZA_COLORS+=":*.rs=38;5;88"
 export EZA_COLORS
 
-alias ls="eza --icons --git"
+alias ls="eza -F --icons --git"
 
 alias cd_gtop='cd `git top`'
 
