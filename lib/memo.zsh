2233aa730ce2746251c010c4df749ca7e42b3c88
byplayer <byplayer100@gmail.com>
2024-02-21 17:20:20 +0900

    👍 change document directory
    change document directory from orgs to memos.

--- lib/memo.zsh
+++ lib/memo.zsh
@@ -1,19 +1,19 @@
-export MEMO_BASE_DIR=~/docs/org_doc
+export MEMO_BASE_DIR=~/docs/memos
 export MEMO_EDITOR=code
 
-function make_memo () {
+function make_memo() {
     MEMO_BASE_NAME=$1
-    
-    if [ -z "$MEMO_BASE_NAME" ] ; then
+
+    if [ -z "$MEMO_BASE_NAME" ]; then
         echo "usage: make_memo memo_title"
         return 1
     fi
-    
-    if [ ! -d ${MEMO_BASE_DIR}/docs/`date +%Y` ] ; then
-        mkdir ${MEMO_BASE_DIR}/docs/`date +%Y`
+
+    if [ ! -d ${MEMO_BASE_DIR}/docs/$(date +%Y) ]; then
+        mkdir ${MEMO_BASE_DIR}/docs/$(date +%Y)
     fi
-    
-    MEMO_PATH=${MEMO_BASE_DIR}/docs/`date +%Y`/`date +%Y%m%d`_${MEMO_BASE_NAME}.md
+
+    MEMO_PATH=${MEMO_BASE_DIR}/docs/$(date +%Y)/$(date +%Y%m%d)_${MEMO_BASE_NAME}.md
     echo "create and open ${MEMO_PATH}"
     ${MEMO_EDITOR} $MEMO_PATH
 }
