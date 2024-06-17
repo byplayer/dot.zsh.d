87d6cdf968c30c84968934f31ebe3d60cadf75c7
byplayer <byplayer100@gmail.com>
2024-06-17 15:28:39 +0900

    👍 add todoist today's or overdue task close fzf

--- plugins/todoist/todoist.plugin.zsh
+++ plugins/todoist/todoist.plugin.zsh
@@ -3,7 +3,18 @@
 function todoist-close() {
     local task
     local todoist_ids
-    task=$(todoist --color l | fzf) &&
+    todoist sync &&
+        task=$(todoist --color list | fzf) &&
+        todoist_ids=$(echo $task | awk '{print $1;}') &&
+        echo close ${task} &&
+        todoist close $todoist_ids
+}
+
+function todoist-today-close() {
+    local task
+    local todoist_ids
+    todoist sync &&
+        task=$(todoist --color list --filter '(overdue | today)' | fzf) &&
         todoist_ids=$(echo $task | awk '{print $1;}') &&
         echo close ${task} &&
         todoist close $todoist_ids
