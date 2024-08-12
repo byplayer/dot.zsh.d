1d78f6b3b0b542570230752dc78d5cb658400c7c
byplayer <byplayer100@gmail.com>
2024-08-12 17:52:10 +0900

    👍 add .tmp to eza color
    .tmp is assign white color in eza default so I change it.

--- lib/misc.zsh
+++ lib/misc.zsh
@@ -59,6 +59,7 @@ EZA_COLORS+=":cr=38;5;28"
 EZA_COLORS+=":*.rb=38;5;124:*.md=38;5;52"
 EZA_COLORS+=":*.rs=38;5;88"
 EZA_COLORS+=":*.bk=38;5;245"
+EZA_COLORS+=":*.tmp=38;5;245"
 export EZA_COLORS
 
 alias ls="eza -g --icons --git"
