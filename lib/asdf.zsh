a09e20f102b9627a45bb832d0252b67fda7569ab
byplayer <byplayer100@gmail.com>
2025-02-06 13:29:31 +0900

    👍 update asdf configuration

--- lib/asdf.zsh
+++ lib/asdf.zsh
@@ -1,5 +1,4 @@
 type brew >/dev/null
 if [ $? -eq 0 ]; then
     export PATH="$(brew --prefix asdf)/bin:${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
-    fpath=(/opt/homebrew/opt/asdf/share/zsh/site-functions $fpath)
 fi
