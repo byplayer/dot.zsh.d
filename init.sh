e6555c1bb5caa5b44a160c58553d31bbabacbd49
byplayer <byplayer100@gmail.com>
2025-05-23 00:55:57 +0900

    Add homebrew path

--- init.sh
+++ init.sh
@@ -3,6 +3,8 @@ export ZSH_EXT_BASE=$HOME/.zsh.d
 autoload -U compinit
 compinit -i
 
+export PATH=/opt/homebrew/bin:$PATH
+
 # TIP: Add files you don't want in git to .gitignore
 for config_file ($ZSH_EXT_BASE/lib/*.zsh) source $config_file
 
