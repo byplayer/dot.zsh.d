2d1826946106ac30a13d897af985c5ffb37fbd3b
byplayer <byplayer100@gmail.com>
2025-12-02 16:12:20 +0900

    ⏪ revert: Revert "Merge pull request #18 from byplayer/feature/improve-zsh-abbr-prefixes"
    This reverts commit 317ee9ad9eade69f6cbe01526436817d8d14897f, reversing
    changes made to 1a8fa77a026a5cacb86d988a54289ee3db7e0267.

--- lib_after_plugin/misc.zsh
+++ lib_after_plugin/misc.zsh
@@ -86,16 +86,9 @@ function ymd_date() {
 export PATH=/opt/emacs/bin:${PATH}
 
 ABBR_REGULAR_ABBREVIATION_GLOB_PREFIXES+=(
-  ' '
   '*& '
   '*&& '
-  '*[|] '
-  '*[|][|] '
+  '*| '
+  '*|| '
   '*; '
-  '[A-Z]*=* '
-)
-
-ABBR_REGULAR_ABBREVIATION_SCALAR_PREFIXES+=(
-  'watch ' 'which ' 'sudo '
-  'env ' 'time ' 'nice ' 'nohup '
 )
